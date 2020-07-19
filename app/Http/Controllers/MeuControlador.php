44aed3ab5b182df7f12ea2614b1867cbcf1a80ae
jordan.finatto <jordan.finatto@universo.univates.br>
2020-07-19 20:46:49 -0300

    Updated routes and views

--- app/Http/Controllers/MeuControlador.php
+++ app/Http/Controllers/MeuControlador.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class MeuControlador extends Controller
 {
-    //
+    public function produtos(){
+        echo "<h1>Produtos</h1>";
+        echo "<ol>";
+        echo "<li>Notebook</li>";
+        echo "<li>Impressora</li>";
+        echo "<li>Mouse</li>";
+        echo "</ol>";
+    }
+
+    public function getNome(){
+        return "Jose da SIlva";
+    }
+
+    public function getIdade(){
+        return "30";
+    }
+
+    public function multiplicar($n1, $n2){
+        return $n1 * $n2;
+    }
 }

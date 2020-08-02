1b396d46a3089d72f76d2db8d9282932d625aba2
jordan.finatto <jordan.finatto@universo.univates.br>
2020-08-02 13:44:13 -0300

    updated migrations

--- database/migrations/2020_08_01_235716_create_departments.php
+++ database/migrations/2020_08_01_235716_create_departments.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBrands extends Migration
+class CreateDepartments extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateBrands extends Migration
      */
     public function up()
     {
-        Schema::create('brands', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ class CreateBrands extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('brands');
+        Schema::dropIfExists('departments');
     }
 }
